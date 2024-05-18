39f20e34aa9d75422d5c90f22a3feebdeaafea0d
lithium582 <angelmamberto15@hotmail.com>
2024-05-18 11:26:00 -0300

    Microdesafio clase 9

--- Data_Engineering/Docker/hola_mundo.py
+++ Data_Engineering/Docker/hola_mundo.py
@@ -1,1 +1,7 @@
 print("HOLA MUNDO")
+
+# docker build -t hola_mundo_imagen .
+# docker run hola_mundo_imagen
+# docker ps -> Contenedores coriendo
+# docker ps -a -> Historial de contenedores
+# docker rm [id] -> remove
