e7f751b7ec8a25c421a8edee351a3b861da38904
lithium582 <angelmamberto15@hotmail.com>
2024-05-31 08:33:51 -0300

    Dags con Airflow

--- Data_Engineering/Docker/Airflow/main.py
+++ Data_Engineering/Docker/Airflow/main.py
@@ -1,5 +1,8 @@
 # curl -O docker-compose.yaml 'https://airflow.apache.org/docs/apache-airflow/2.3.3/docker-compose.yaml'
 # https://github.com/CoderContenidos
 
+# Descarga de imágenes y correr containers
 # docker-compose up airflow-init
+
+# Comando final
 # docker-compose up
